780c7a6ad77e9ad9711926c5a3bd4eb347fc72d2
thisisteey <dadataiwo82@yahoo.com>
2024-06-25 18:28:27 +0000

    Added

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,71 +1,73 @@
-#!/usr/bin/python3
-""" LFU Caching """
+#!/usr/bin/env python3
+"""Least Frequently Used caching module.
+"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching """
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LFU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Constructor """
+        """Initializes the cache.
+        """
         super().__init__()
-        self.queue = []
-        self.counter = {}
+        self.cache_data = OrderedDict()
+        self.keys_freq = []
+
+    def __reorder_items(self, mru_key):
+        """Reorders the items in this cache based on the most
+        recently used item.
+        """
+        max_positions = []
+        mru_freq = 0
+        mru_pos = 0
+        ins_pos = 0
+        for i, key_freq in enumerate(self.keys_freq):
+            if key_freq[0] == mru_key:
+                mru_freq = key_freq[1] + 1
+                mru_pos = i
+                break
+            elif len(max_positions) == 0:
+                max_positions.append(i)
+            elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
+                max_positions.append(i)
+        max_positions.reverse()
+        for pos in max_positions:
+            if self.keys_freq[pos][1] > mru_freq:
+                break
+            ins_pos = pos
+        self.keys_freq.pop(mru_pos)
+        self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
 
     def put(self, key, item):
-        """ Puts item in cache """
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        self.cache_data[key] = item
-
-        item_count = self.counter.get(key, None)
-
-        if item_count is not None:
-            self.counter[key] += 1
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lfu_key, _ = self.keys_freq[-1]
+                self.cache_data.pop(lfu_key)
+                self.keys_freq.pop()
+                print("DISCARD:", lfu_key)
+            self.cache_data[key] = item
+            ins_index = len(self.keys_freq)
+            for i, key_freq in enumerate(self.keys_freq):
+                if key_freq[1] == 0:
+                    ins_index = i
+                    break
+            self.keys_freq.insert(ins_index, [key, 0])
         else:
-            self.counter[key] = 1
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.get_first_list(self.queue)
-            if first:
-                self.queue.pop(0)
-                del self.cache_data[first]
-                del self.counter[first]
-                print("DISCARD: {}".format(first))
-
-        if key not in self.queue:
-            self.queue.insert(0, key)
-        self.mv_right_list(key)
+            self.cache_data[key] = item
+            self.__reorder_items(key)
 
     def get(self, key):
-        """ Gets item from cache """
-        item = self.cache_data.get(key, None)
-        if item is not None:
-            self.counter[key] += 1
-            self.mv_right_list(key)
-        return item
-
-    def mv_right_list(self, item):
-        """ Moves element to the right, taking into account LFU """
-        length = len(self.queue)
-
-        idx = self.queue.index(item)
-        item_count = self.counter[item]
-
-        for i in range(idx, length):
-            if i != (length - 1):
-                nxt = self.queue[i + 1]
-                nxt_count = self.counter[nxt]
-
-                if nxt_count > item_count:
-                    break
-
-        self.queue.insert(i + 1, item)
-        self.queue.remove(item)
-
-    @staticmethod
-    def get_first_list(array):
-        """ Get first element of list or None """
-        return array[0] if array else None
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.__reorder_items(key)
+        return self.cache_data.get(key, None)
