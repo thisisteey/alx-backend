3a6f04c24ee9ce4eed355cae50035bbba3fb287f
thisisteey <dadataiwo82@yahoo.com>
2024-07-02 11:41:06 +0000

    Feature: Install Flask-Babel and configure app with English and French locales

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module for a basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config:
+    """Class for the flask_babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index() -> str:
+    """Index page of the Flask application"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
