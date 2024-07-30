89c0c3bb338ef123ca563e48aff37b3a2c3c8dc2
thisisteey <dadataiwo82@yahoo.com>
2024-07-30 13:59:09 +0000

    Feature: Add job creation script to enqueue multiple notifications with detailed logging in 7-job_creator.js

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,69 @@
+import { createQueue } from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account',
+  },
+];
+
+const notifQueue = createQueue({ name: 'push_notification_code_2' });
+
+for (const jobData of jobs) {
+  const notifJob = notifQueue.create('push_notification_code_2', jobData);
+
+  notifJob
+    .on('enqueue', () => {
+      console.log('Notification job created:', notifJob.id);
+    })
+    .on('complete', () => {
+      console.log('Notification job', notifJob.id, 'completed');
+    })
+    .on('failed', (err) => {
+      console.log('Notification job', notifJob.id, 'failed:', err.message || err.toString());
+    })
+    .on('progress', (progress) => {
+      console.log('Notification job', notifJob.id, `${progress}% complete`);
+    });
+  notifJob.save();
+}
