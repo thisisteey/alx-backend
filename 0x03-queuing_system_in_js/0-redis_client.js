2cafb0dc91ee1c3a5a626940c7174d31ad1ba74e
thisisteey <dadataiwo82@yahoo.com>
2024-07-30 11:45:20 +0000

    Feature: Install node_redis and add 0-redis_client.js to connect and log Redis server status

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
